21431c42d937b46326936f2281d4a81baa96f80b
Vijayakb <vijayakb@Vijayakbs-MacBook-Pro.local>
2021-05-02 09:31:39 +0530

    Array geeks

---
+++ arrays/README.md
@@ -0,0 +1,2 @@
+Question 1: Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements. 
+
---
+++ arrays/util.go
@@ -0,0 +1,8 @@
+package main
+
+
+func printArray(a []int){
+	for _, value:= range a{
+		println(value)
+	}
+}
